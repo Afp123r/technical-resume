b933e637b971679910e986e292e8e1139ccf14cf
Afp123r <a19561897@gmail.com>
2025-06-21 22:52:41 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Free web developer resume templates",
+  title: "Portfolio",
   description: "Premium web developer resume templates",
 };
 
